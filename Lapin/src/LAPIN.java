0e7aaf60cd60a4be8da33911927db28ef412ed75
emperoralz97 <emperorabbas@gmail.com>
2018-05-01 17:54:23 +0530

    1st changes

--- Lapin/src/LAPIN.java
+++ Lapin/src/LAPIN.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
 
 public class LAPIN {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		int t;
+		t = in.nextInt();
+		while(t>0) {
+			boolean flg = false;
+			String s = in.next();
+			int len = s.length();
+			if(len%2==0) {
+				if(s.substring(0, len/2).equals(s.substring(len/2)))flg = true;
+			}else {
+				if(s.substring(0, len/2).equals(s.substring(len/2+1)))flg = true;
+			}
+			
+			if(flg)System.out.println("YES");
+			else
+				System.out.println("NO");
+			t--;
+		}
+		
 
 	}
 
